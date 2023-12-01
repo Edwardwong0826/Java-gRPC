1a08835356736822574c67c566f9ff944ff3b1ed
Edwardwong0826 <edwardwong_94@outlook.com>
2023-12-01 17:55:43 +0800

    Protobuf with gRPC
    Example of using protobuf file to generated java  gRPC file with details.

---
+++ src/main/java/com/wong/grpc/Main.java
@@ -0,0 +1,25 @@
+package com.wong.grpc;
+
+//import org.example.protobuf.AddressBookProtos;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        int id = new Random().nextInt();
+//        String name = "Test";
+//        String email = "test@example.org";
+//        String number = "01234567890";
+//        AddressBookProtos.Person person = AddressBookProtos.Person.newBuilder()
+//                .setId(id)
+//                .setName(name)
+//                .setEmail(email)
+//                .addNumbers(number)
+//                .build();
+//        AddressBookProtos.AddressBook addressBook = AddressBookProtos.AddressBook.newBuilder()
+//                .addPeople(person)
+//                .build();
+//        System.out.println(addressBook);
+    }
+}
